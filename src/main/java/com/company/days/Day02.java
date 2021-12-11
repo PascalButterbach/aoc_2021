2a7749fafbf1792249c40499c5d1ada9ca87738a
PascalButterbach <pascal@butterbach.org>
2021-12-11 12:24:38 +0100

    less lines = better

--- src/main/java/com/company/days/Day02.java
+++ src/main/java/com/company/days/Day02.java
@@ -15,7 +15,8 @@ public class Day02 extends BaseDay {
         Map<String, Long> collect = getInput().stream()
                 .map(s -> s.split(" "))
                 .collect(Collectors.groupingBy(strings -> strings[0], Collectors.mapping(t -> t[1], Collectors.summarizingInt(Integer::parseInt))))
-                .entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, s -> s.getValue().getSum()));
+                .entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, s -> s.getValue().getSum()));
 
        return collect.get("forward") * (Math.max(collect.get("up"), collect.get("down")) - Math.min(collect.get("up"), collect.get("down")));
     }
