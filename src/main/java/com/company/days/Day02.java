e8566a54cfb2b69976e525755d73fe78d3b41c61
PascalButterbach <pascal@butterbach.org>
2021-12-11 12:23:04 +0100

    less lines = better

--- src/main/java/com/company/days/Day02.java
+++ src/main/java/com/company/days/Day02.java
@@ -12,15 +12,12 @@ public class Day02 extends BaseDay {
     @Override
     public Object solvePartOne() {
 
-        Map<String, Integer> collect = getInput().stream()
+        Map<String, Long> collect = getInput().stream()
                 .map(s -> s.split(" "))
-                .collect(Collectors.groupingBy(strings -> strings[0], Collectors.mapping(t -> t[1], Collectors.toList())))
-                .entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, stringListEntry -> stringListEntry.getValue().stream()
-                        .mapToInt(Integer::parseInt)
-                        .sum()));
+                .collect(Collectors.groupingBy(strings -> strings[0], Collectors.mapping(t -> t[1], Collectors.summarizingInt(Integer::parseInt))))
+                .entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, s -> s.getValue().getSum()));
 
-        return collect.get("forward") * (Math.max(collect.get("up"), collect.get("down")) - Math.min(collect.get("up"), collect.get("down")));
+       return collect.get("forward") * (Math.max(collect.get("up"), collect.get("down")) - Math.min(collect.get("up"), collect.get("down")));
     }
 
     @Override
